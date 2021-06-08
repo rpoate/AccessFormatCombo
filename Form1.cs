e782bc5a23514fd0b361202ad8a25770d28783fa
zoople.tech <info@zoople.tech>
2021-06-08 12:53:10 +1000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccessFormatCombo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void htmlEditControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.D1 && e.Control)
+            {
+                ((ToolStripComboBox)this.htmlEditControl1.ToolStripItems["FormatSelectionCombo"]).Text = "Heading 1";
+                ((ToolStripComboBox)this.htmlEditControl1.ToolStripItems["FormatSelectionCombo"]).PerformClick();
+                e.SuppressKeyPress = true;
+            }
+        }
+    }
+}
